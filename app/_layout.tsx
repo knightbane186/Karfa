7138dcd48f65646b589366a04c7fc961e2387314
knightbane186 <denverlopes93@gmail.com>
2024-07-02 23:15:25 +1000

    setting up initial again

---
+++ app/_layout.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const rootLayout = () => {
+  return (
+    <View>
+      <Text> rootLayout </Text>
+    </View>
+  )
+}
+
+export default rootLayout
+
+const styles = StyleSheet.create({})
