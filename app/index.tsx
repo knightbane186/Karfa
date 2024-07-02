aa1f55b86e42f85522ca4c4b831906600de32019
knightbane186 <denverlopes93@gmail.com>
2024-07-02 23:37:57 +1000

    successfully added native

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Karfaaa</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Karfaaa</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-  },
-});
