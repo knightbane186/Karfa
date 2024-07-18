9ac7a0ecd1af5b0bb717599a7bcaa660eb3c2d70
knightbane186 <denverlopes93@gmail.com>
2024-07-18 16:13:08 +1000

    adding past

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import {Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 
 
@@ -8,6 +9,8 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-black">
       
       <Text className="text-3xl text-green-600 font-Inter-variable">Frogit</Text>
+      <Link href="/profile" style={{color:'blue'}}>go to profile
+      </Link>
       <StatusBar style="auto" />
     </View>
   );
