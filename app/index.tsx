9f074c0486e0161564db5231c5620cf4939aefd2
knightbane186 <denverlopes93@gmail.com>
2024-07-18 16:10:33 +1000

    :

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import {Text, View } from 'react-native';
 
+
+
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-3xl text-green-600 font-inter">Frogit</Text>
+      
+      <Text className="text-3xl text-green-600 font-Inter-variable">Frogit</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
