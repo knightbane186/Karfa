09af817d0490746858baae32022ee2f63d3ac8d2
knightbane186 <denverlopes93@gmail.com>
2024-07-22 02:40:36 +1000

    adding login page

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
 const Authlayout = () => {
   return (
@@ -18,8 +19,10 @@ options={{
   headerShown:false,
 }}
 />
-
  </Stack>
+ <StatusBar backgroundColor='#161622'
+ 
+ />
  
  </>
   )
--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import image
 
 const signIn = () => {
   return (
-    <View>
-      <Text>signIn</Text>
-    </View>
-  )
+<SafeAreaView className="bg-primary h-full">
+<ScrollView>
+  <View className="w-full justify-center min-h-[85vh] px-4 my-6" >
+
+  </View>
+</ScrollView>
+
+</SafeAreaView>
 }
 
 export default signIn
