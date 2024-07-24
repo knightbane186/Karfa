1878f71b97b8d4379b8e984ade43c0ea065c0148
knightbane186 <denverlopes93@gmail.com>
2024-07-24 22:51:02 +1000

    adding basics

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,19 +1,17 @@
 import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import image
+
 
 const signIn = () => {
   return (
 <SafeAreaView className="bg-primary h-full">
 <ScrollView>
   <View className="w-full justify-center min-h-[85vh] px-4 my-6" >
-
+//you can insert image here 
   </View>
 </ScrollView>
 
 </SafeAreaView>
 }
-adding the basic
-//this feesl so lowkey stupid, that i cant even focus on th ebascis 
 export default signIn
