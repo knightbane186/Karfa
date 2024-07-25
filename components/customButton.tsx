36d9dc575a1b367b47c00b0bb4facfc0b4f4b361
knightbane186 <denverlopes93@gmail.com>
2024-07-25 18:40:36 +1000

    back to sqaure one

--- components/customButton.tsx
+++ components/customButton.tsx
@@ -1,14 +1,19 @@
-import { View, Text,  TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const customButton = () => {
+const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoading }) => {
   return (
-<TouchableOpacity className={`w-full bg-red rounded-md py-3 mb-4 flex-row justify-center items-center`}>
-<Text>
-    CustomButton
-</Text>
-</TouchableOpacity>
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`w-full bg-red rounded-md py-3 mb-4 flex-row justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
+      disabled={isLoading}
+    >
+      <Text className={`text-white font-semibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
   )
 }
 
-export default customButton
+export default CustomButton
