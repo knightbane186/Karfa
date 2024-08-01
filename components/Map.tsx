fc08b0505e640ad09f01e158d769b8312f669ecc
knightbane186 <denverlopes93@gmail.com>
2024-08-01 18:54:04 +1000

    adding the live location

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,4 +1,72 @@
 
+// this sinmore accurate but lets see
+
+// import React, { useEffect, useState } from 'react';
+// import { StyleSheet, View, Text } from 'react-native';
+// import Mapbox, { Camera, MapView, LocationPuck } from '@rnmapbox/maps';
+// import * as Location from 'expo-location';
+
+// const accessToken = 'pk.eyJ1IjoiZGVudmVyMCIsImEiOiJjbHo2cXNsMXYwMTd2MmxxMHRoN2luaWVuIn0.5MQnpgOV6Nv4DfCeXppPnw';
+// Mapbox.setAccessToken(accessToken);
+
+// // Define the type for location
+// type LocationType = Location.LocationObject | null;
+
+// export default function Map() {
+//     const [location, setLocation] = useState<LocationType>(null);
+//     const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+//     useEffect(() => {
+//         (async () => {
+//             let { status } = await Location.requestForegroundPermissionsAsync();
+//             if (status !== 'granted') {
+//                 setErrorMsg('Permission to access location was denied');
+//                 return;
+//             }
+
+//             let currentLocation = await Location.getCurrentPositionAsync({});
+//             console.log(currentLocation); // Debugging log
+//             setLocation(currentLocation);
+//         })();
+//     }, []);
+
+//     return (
+//         <View style={styles.container}>
+//             {errorMsg ? <Text>{errorMsg}</Text> : null}
+//             <MapView
+//                 style={styles.map}
+//                 styleURL="mapbox://styles/mapbox/outdoors-v12"
+//                 zoomEnabled={true}
+//                 scrollEnabled={true}
+//                 pitchEnabled={true}
+//                 rotateEnabled={true}
+//             >
+//                 {location && (
+//                     <Camera
+//                         centerCoordinate={[location.coords.longitude, location.coords.latitude]}
+//                         zoomLevel={14}
+//                         animationMode={'flyTo'}
+//                         animationDuration={0}
+//                     />
+//                 )}
+//                 <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
+//             </MapView>
+//         </View>
+//     );
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//     },
+//     map: {
+//         flex: 1,
+//     },
+// });
+
+
+
+
 import React from 'react';
 import { StyleSheet, View, } from 'react-native';
 import Mapbox, { Çamera, MapView, Camera, LocationPuck } from '@rnmapbox/maps';
