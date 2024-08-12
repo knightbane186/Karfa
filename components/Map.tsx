c5be71bc7dd35e657f8b4b8eb69d6366e63963e3
knightbane186 <denverlopes93@gmail.com>
2024-08-12 22:46:19 +1000

    map and tabs bar working

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,22 +1,27 @@
-
 import React from 'react';
-import { StyleSheet, View, } from 'react-native';
-import Mapbox, { Çamera, MapView, Camera, LocationPuck } from '@rnmapbox/maps';
+import { StyleSheet, View } from 'react-native';
+import Mapbox, { Camera, MapView, LocationPuck } from '@rnmapbox/maps';
 
-const accessToken = 'pk.eyJ1IjoiZGVudmVyMCIsImEiOiJjbHo2cXNsMXYwMTd2MmxxMHRoN2luaWVuIn0.5MQnpgOV6Nv4DfCeXppPnw';
+const accessToken = 'pk.eyJ1IjoiZGVudmVyMCIsImEiOiJjbHpxeXU1aHMwMTk2MmxvbjRqbzRmeWpyIn0.NZ-Xjxx7L5ARWfPkDm0a6A';
 Mapbox.setAccessToken(accessToken);
 
 export default function Map() {
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} styleURL="mapbox://styles/mapbox/outdoors-v12"
-            zoomEnabled={true}
-            scrollEnabled={true}
+            <MapView 
+                style={styles.map} 
+                styleURL="mapbox://styles/mapbox/outdoors-v12"
+                zoomEnabled={true}
+                scrollEnabled={true}
                 pitchEnabled={true}
                 rotateEnabled={true}>
-               <Camera followUserLocation /> 
-               <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{isEnabled:true}}/>
-                </MapView>
+                <Camera followUserLocation /> 
+                <LocationPuck 
+                    puckBearingEnabled 
+                    puckBearing="heading" 
+                    pulsing={{ isEnabled: true }} 
+                />
+            </MapView>
         </View>
     );
 }
@@ -29,5 +34,3 @@ const styles = StyleSheet.create({
         flex: 1,
     },
 });
-
-
